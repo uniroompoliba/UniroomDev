ccb61b1a47f14e9a4b5231224dab53087d16c9f4
ferr34 <ferrantealdo194@gmail.com>
2020-11-13 21:13:17 +0100

    modifica #9 - settaggio db da modificare - in arrivo domani la modifica #10

--- app/src/main/java/com/example/uniroomf/MainActivity.kt
+++ app/src/main/java/com/example/uniroomf/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.uniroomf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.util.Properties;
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Colleghiamo il database all'activity
+        val connectionProps = Properties()
+        connectionProps.put("user","uniroompoliba")
+        connectionProps.put("password","rktFpXge5rFG")
+
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance()
+           var conn : Connection? = DriverManager.getConnection("jdbc:mysql:ftp.uniroompoliba.altervista.org:21/",connectionProps)
+            print("Connessione correttamente riuscita")
+        }
+        catch (ex: SQLException )
+        {
+            ex.printStackTrace();
+        }
+
     }
 
 }
