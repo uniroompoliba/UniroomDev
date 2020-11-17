fbcea73a07c82cb3ae2184a80dce8d07b6063731
ferr34 <ferrantealdo194@gmail.com>
2020-11-17 13:36:43 +0100

    modifica #12 di aldo

--- app/src/main/java/com/example/uniroomf/MainActivity.kt
+++ app/src/main/java/com/example/uniroomf/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uniroomf
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.json.JSONArray
@@ -11,6 +12,8 @@ import java.sql.DriverManager
 import java.sql.SQLException
 import java.util.Properties;
 import java.net.*;
+import android.view.*;
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,14 @@ class MainActivity : AppCompatActivity() {
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Imposto i listener per andare alle successive activity - regBtn
+        var regBtn =  findViewById<Button>(R.id.regbutt)
+
+        regBtn.setOnClickListener{
+             val intent = Intent(this,regActivity::class.java)
+            startActivity(intent) //teoricamente cosi dovrei avviare l'altra activity
+         }
 
 
     }
