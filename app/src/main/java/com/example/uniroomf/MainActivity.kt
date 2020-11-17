0d8c76ee20bcc87b5569e869e37bddeb770fef0b
ferr34 <ferrantealdo194@gmail.com>
2020-11-17 13:14:33 +0100

    modifica #11 di aldo

--- app/src/main/java/com/example/uniroomf/MainActivity.kt
+++ app/src/main/java/com/example/uniroomf/MainActivity.kt
@@ -2,33 +2,25 @@ package com.example.uniroomf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.json.JSONArray
+import java.io.InputStream
+import java.io.OutputStreamWriter
+import java.lang.StringBuilder
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 import java.util.Properties;
+import java.net.*;
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Colleghiamo il database all'activity
-        val connectionProps = Properties()
-        connectionProps.put("user","uniroompoliba")
-        connectionProps.put("password","rktFpXge5rFG")
-
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver").newInstance()
-           var conn : Connection? = DriverManager.getConnection("jdbc:mysql:ftp.uniroompoliba.altervista.org:21/",connectionProps)
-            print("Connessione correttamente riuscita")
-        }
-        catch (ex: SQLException )
-        {
-            ex.printStackTrace();
-        }
 
     }
+
 
 }
