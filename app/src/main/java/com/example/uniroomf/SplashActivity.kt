e05dcf5ddae5bf4db89a39d63827e5e0849006b0
daven <davidenusco@gmail.com>
2020-11-21 19:28:45 +0100

    Aggiunta splash screen

---
+++ app/src/main/java/com/example/uniroomf/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.uniroomf
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
+
