1a24bb0faee5dd8cf374ea2b4a4cd67443243ca7
Stefano <stefano99paparella@gmail.com>
2020-12-02 17:05:38 +0100

    BRODY GUARDA QUESTA CHE BODY

---
+++ app/src/main/java/com/example/uniroomf/menuPrincActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uniroomf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menuPrincActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_princ)
+    }
+}
