b094b1e276637d89631c66f3f3f5db5d5c2322d1
Stefano <stefano99paparella@gmail.com>
2020-12-02 12:32:55 +0100

    LE VELINEEEEEEEEEEEEEEEE

---
+++ app/src/main/java/com/example/uniroomf/princStudActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uniroomf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class princStudActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_princ_stud)
+    }
+}
