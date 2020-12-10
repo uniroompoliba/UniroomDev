fbd093b9bc6ce2697694364fe4b513cbad3f46ba
ferr34 <ferrantealdo194@gmail.com>
2020-12-10 15:23:02 +0100

    Aggiunti listener ai bottoni per nuova prenotazione e impostazioni

--- app/src/main/java/com/example/uniroomf/menuPrincActivity.kt
+++ app/src/main/java/com/example/uniroomf/menuPrincActivity.kt
@@ -1,11 +1,40 @@
 package com.example.uniroomf
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class menuPrincActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_princ)
+    }
+
+    override fun onResume() {
+        super.onResume() // Costruttore richiamato dalla super classe
+        val bundle : Bundle? = intent.extras
+        var ruolo = bundle!!.getString("ruolo")
+
+
+        // Aggiungo il listener della nuova prenotazione
+        var addPren = findViewById<Button>(R.id.newPren)
+
+        addPren.setOnClickListener {
+            //Passaggio alla activity della nuova prenotazione
+            var intentPassNewPren = Intent(this,prenotazStudActivity::class.java)
+            startActivity(intentPassNewPren)
+        }
+
+
+        //Aggiungo il listener al bottone delle impostazioni
+        var settBtn = findViewById<Button>(R.id.settings)
+
+        settBtn.setOnClickListener {
+            //Passaggio alla activity delle impostazioni
+            var intentPassImp = Intent(this,acivityImpostazioni::class.java)
+            intentPassImp.putExtra("ruolo",ruolo)
+            startActivity(intentPassImp)
+        }
     }
 }
