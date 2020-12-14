44764f75462afb7518b1d52d633b78e960f3ab24
Stefano <stefano99paparella@gmail.com>
2020-12-14 18:36:31 +0100

    ***AGGIORNAMENTO 14/12/2020 (ORE 18:36)*** - CREATA LA PAGINA DI STORICO - HO COLLEGATO LA PAGINA DI RECAP A QUELLA DEL MENU PRINCIPALE. - STO CERCANDO DI SISTEMARE I TESTI NELLA DARK MODE. LI STO FACENDO BIANCHI. - HO CREATO LA PAGINA DI RIEPILOGO (SI DICEVA RIEPILOGO, PORCODIO). COMUNQUE SIA,   IN recapAvtivity CI SONO TUTTE LE ISTRUZIONI PER MODIFICARE LE TEXTVIEW. BUONA FORTUNA
    ***DA IMPLEMENTARE***
    - CAMARDA CAPO DELLA TRAP, NO CAPPELLO!
    
    ***AVVERTENZE (BY STEFANO)***
    POMPATEVI MATTONI!

---
+++ app/src/main/java/com/example/uniroomf/storicoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uniroomf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class storicoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storico)
+    }
+}
