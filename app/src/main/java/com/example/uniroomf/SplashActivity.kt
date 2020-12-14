ea2b52930f57820216e4b148dde6b51d46e7377d
Stefano <stefano99paparella@gmail.com>
2020-12-14 10:55:43 +0100

    ***AGGIORNAMENTO 14/12/2020 (ORE 10:55)*** - STO CERCANDO DI SISTEMARE I TESTI NELLA DARK MODE. LI STO FACENDO BIANCHI. - SICCOME LA DARK MODE E' NATIVA, PROVVEDERO' A TOGLIERE IL BOTTONE PER ATTIVARLA DALLA PAGINA DELLE IMPOSTAZIONI
    ***DA IMPLEMENTARE***
    -SISTEMARE IL FATTO DELL'HANDLER IN activity_splash.xml
    
    ***AVVERTENZE (BY STEFANO)***
    AL MOMENTO NON CI SONO ALTRE AVVERTENZE.

--- app/src/main/java/com/example/uniroomf/SplashActivity.kt
+++ app/src/main/java/com/example/uniroomf/SplashActivity.kt
@@ -2,10 +2,14 @@ package com.example.uniroomf
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.android.HandlerDispatcher
+import kotlinx.coroutines.delay
 
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
