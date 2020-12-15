c2130b9b0295081ca15283b68f4f0a9922e41a7f
Stefano <stefano99paparella@gmail.com>
2020-12-15 16:18:17 +0100

    ***AGGIORNAMENTO 15/12/2020 (ORE 16:15)*** - l'activity che si chiamava prnotazStudActivity ora si chiama prenotazDocActivity, poiché per gli studenti io e Francesco abbiamo studiato un altro metodo di prenotazione. Non preoccupatevi: tutte le occorrenze sono stare rinominate in automatico. Adesso, prenotazStudActivity è vuota, perché dobbiamo mettere la lista di prenotazioni fatte.
    ***DA IMPLEMENTARE***
    - prenotazStudActivity.kt
    - Adapter
    
    ***AVVERTENZE (BY STEFANO)***
    PER GIORGIO: MUOVITI CON IL LOGIN PERCHE' DIOCANE MANCANO 7 GIORNI, QUASI 6, E STIAMO ANCORA A 0.
    PER TUTTI: SALTATE PIU' LEZIONI POSSIBILI E DEDICHIAMOCI H24 AL PROGETTO.

--- app/src/main/java/com/example/uniroomf/prenotazStudActivity.kt
+++ app/src/main/java/com/example/uniroomf/prenotazStudActivity.kt
@@ -1,38 +1,11 @@
 package com.example.uniroomf
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.CalendarView
-import android.widget.Spinner
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class prenotazStudActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prenotaz_stud)
-    }
-
-    override fun onResume() {
-        super.onResume() // Costruttore richiamato dalla superclasse
-
-        // Prendo i valori degli elementi grafici
-        var spinAula = findViewById<Spinner>(R.id.spinnerAule);
-        var aula = spinAula.selectedItem.toString()
-
-        var dataPren = findViewById<CalendarView>(R.id.calendPrenS).date;
-        //TODO
-
-        // Reset listener
-        var rstBtn = findViewById<Button>(R.id.resPrenS)
-
-        rstBtn.setOnClickListener {
-            //Imposto dei valori di partenza
-
-        }
-
     }
 }
