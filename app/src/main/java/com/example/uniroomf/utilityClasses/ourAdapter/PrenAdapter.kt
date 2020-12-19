d8afcc4b5cbfcd5153b6c73fe31a1b08be2ce0d5
gianf <gianferrinogiorgio@gmail.com>
2020-12-19 16:20:14 +0100

    a

--- app/src/main/java/com/example/uniroomf/utilityClasses/ourAdapter/PrenAdapter.kt
+++ app/src/main/java/com/example/uniroomf/utilityClasses/ourAdapter/PrenAdapter.kt
@@ -7,10 +7,11 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import com.example.uniroomf.R
+import com.example.uniroomf.storicoDocActivity
 import com.example.uniroomf.storicoStudActivity
 import com.example.uniroomf.utilityClasses.PrenInfo
 
-class PrenAdapter(storicoDocActivity: storicoStudActivity, generaPrenotazioni: List<PrenInfo>) : BaseAdapter()
+class PrenAdapter(storicoDocActivity: storicoDocActivity, generaPrenotazioni: List<PrenInfo>) : BaseAdapter()
 {
     // Elementi dell'adapter
     private var pren : List<PrenInfo>? = null
