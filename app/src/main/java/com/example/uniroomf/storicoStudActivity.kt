0ea91670fcdf6f8dd935904fcf57cee83ac5bf11
gianf <gianferrinogiorgio@gmail.com>
2020-12-19 17:34:31 +0100

    NO

--- app/src/main/java/com/example/uniroomf/storicoStudActivity.kt
+++ app/src/main/java/com/example/uniroomf/storicoStudActivity.kt
@@ -2,6 +2,8 @@ package com.example.uniroomf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Adapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Spinner
@@ -9,56 +11,47 @@ import com.android.volley.toolbox.Volley
 import com.example.uniroomf.utilityClasses.PrenInfo
 import com.example.uniroomf.utilityClasses.ourAdapter.PrenAdapter2
 
-class storicoStudActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class storicoStudActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_storico_stud)
     }
 
-    override fun onResume()
-    {
+    override fun onResume() {
         super.onResume()
-        val settBtn = findViewById<Button>(R.id.settings)
-
-        fun generaPrenotazioni() : List<PrenInfo>
+        val setBtn = findViewById<Button>(R.id.settings)
+        fun generaPrenotazione() : List<PrenInfo>
         {
             var listaElementi : List<PrenInfo>? = null;
 
             // Request per fare il retrieval delle query
+            var myRQ = Volley.newRequestQueue(this)
+            var urlPrenDoc = "http://uniroompoliba.altervista.org/public/ScriptPrenotazioni/estraiPrenDoc.php"
 
 
-
-
-            return listaElementi!!
+            return listaElementi !!
         }
 
-        var adapter = PrenAdapter2(this ,generaPrenotazioni() )
-        settBtn.setOnClickListener {
-            var aula = findViewById<Spinner>(R.id.spinnerTipologia).textAlignment.toString()
-            var docente = findViewById<Spinner>(R.id.spinnerDocente).textAlignment.toString()
+        var Adapter = PrenAdapter2(this, generaPrenotazione())
+        setBtn.setOnClickListener {
+            val aula = findViewById<Spinner>(R.id.spinnerTipologia).textAlignment.toString()
+            val docente = findViewById<Spinner>(R.id.spinnerDocente).textAlignment.toString()
             var matST = findViewById<Spinner>(R.id.spinnerMateria).textAlignment.toString()
             var listStorST = findViewById<ListView>(R.id.listaStorStud)
-                    listStorST.setAdapter(adapter)
+            listStorST.setAdapter(Adapter)
 
 
-                //definire eventi comunicazione
-                var url = "http://uniroompoliba.altervista.org/public/accesso.php"
-                var myRQ = Volley.newRequestQueue(this)
+            //definire eventi comunicazione
+            var url = "http://uniroompoliba.altervista.org/public/accesso.php"
+            var myRQ = Volley.newRequestQueue(this)
 
-                fun setMyHeader()
-                {
-                    var headers = HashMap<String, String>()
-                    headers.put("Content-Type", "application/json")
-                    headers.put("Accept", "application/json")
-                }
-
-
+            fun setMyHeader()
+            {
+                var headers = HashMap<String, String>()
+                headers.put("Content-Type", "application/json")
+                headers.put("Accept", "application/json")
             }
-
-
-
-
-
-
         }
+
     }
+}
